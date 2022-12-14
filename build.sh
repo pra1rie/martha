f7dcdc3027b96b780eeee95d6a60e2af295d7904
Ren <73724137+pra1rie@users.noreply.github.com>
2022-12-14 04:06:31 +0000

    got bored of shared lib

--- build.sh
+++ build.sh
@@ -5,13 +5,8 @@
 NAME='bin/martha'
 LIBS='-lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf'
 
-# lmao agian
-cd nara/
-./build.sh
-cd ..
-
 rm -f $NAME
-gcc sauce/*.c -I nara/include -Lnara -lnara $LIBS -o $NAME
+gcc nara/*.c sauce/*.c -I nara $LIBS -o $NAME
 rm -rf *.o
 
 [ -f $NAME ] && ./$NAME
