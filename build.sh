b0eb981a9abe665b52dfbd1b25c40c936638512b
Ren <73724137+pra1rie@users.noreply.github.com>
2022-12-14 04:18:02 +0000

    modified build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ NAME='martha'
 LIBS='-lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf'
 
 rm -f bin/$NAME
-gcc nara/*.c sauce/*.c -I nara $LIBS -o bin/$NAME
+gcc sauce/*.c nara/*.c -I nara $LIBS -o bin/$NAME
 rm -rf *.o
 
 [ -f bin/$NAME ]; { cd bin; ./$NAME; }
